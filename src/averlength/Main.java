74e8e4f76272571be2b8de3b633824a0a5e3e2d4
BoWang2 <wangbo2541@gmail.com>
2018-03-02 02:35:58 -0500

    use charAt() to distinguish different char

---
+++ src/averlength/Main.java
@@ -0,0 +1,21 @@
+package averlength;
+
+import java.util.Scanner;  
+
+public class Main {  
+   public static void main(String[] args){  
+       Scanner in =new Scanner(System.in);  
+       String string=null;  
+       string=in.nextLine();  
+       char s=string.charAt(0);  
+       double count=1,d;  
+       for (int i = 0; i < string.length(); i++) {  
+        if (s!=string.charAt(i)) {  
+            s=string.charAt(i);  
+            count++;  
+        }  
+    }  
+       d=string.length()/count;  
+       System.out.println(String.format("%.2f", d));  
+   }  
+}  
